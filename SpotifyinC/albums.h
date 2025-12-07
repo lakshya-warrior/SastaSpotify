5e8f70bca85f8910afce63a65852cebf4c9bfa62
lakshya-warrior <83820036+lakshya-warrior@users.noreply.github.com>
2025-12-07 13:17:42 +0530

    Update albums.h

--- SpotifyinC/albums.h
+++ SpotifyinC/albums.h
@@ -3,11 +3,14 @@
 
 #include "headerfile.h"
 
+extern int numberofalbums; 
+void genalbumcnt();
 int gen_album(char name[]);//
-void albumadd(int index, const char *name, lib_node* library);//
-void albumdel(int index, const char *name, lib_node* library);//
-void list_songs_album(char name[], lib_node* library);//
+void albumname(int idx_to_find);
+void albumadd(int index, int albumidx, lib_node* library);//
+void albumdel(int index, int albumidx, lib_node* library);//
+void list_songs_album(int albumidx, lib_node* library);//
 void list_albums();//
-int* return_songs_album(char name[]);//
+song_list* return_songs_album(int albumidx, lib_node* library, song_list* head);
 
 #endif
