8b9dcb573cc2c7998c3488d0b4e59ad420108c61
lakshya-warrior <83820036+lakshya-warrior@users.noreply.github.com>
2025-12-07 13:18:57 +0530

    Change add_album_playlist parameter from name to index

--- SpotifyinC/playlist.h
+++ SpotifyinC/playlist.h
@@ -8,7 +8,7 @@ song_list* del_playlist(song_list* head, int index, lib_node* library);
 void showpl(song_list* head, lib_node* library);//
 song_list* playnext(song_list* head, lib_node* library);
 song_list* playprev(song_list* head, lib_node* library);
-song_list* add_album_playlist(song_list* head, char name[], lib_node* library);//
+song_list* add_album_playlist(song_list* head, int album_index, lib_node* library);//
 song_list* play(song_list* head, lib_node* library);
 void free_playlist(song_list* head);//
 
